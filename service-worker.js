c322c36197a5d67065a9f5499b381e64f8532a1c
chatxv <135034996+chatxv@users.noreply.github.com>
2024-02-06 15:39:12 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,12 @@
-
+<script>
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('https://news-zacine.com/v2-sw.js')
+        .then(function(registration) {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+          console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+  }
+</script>
